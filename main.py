d7f9404f35e53a79db536f302c8f758c176479bf
regutierrez <11924623+regutierrez@users.noreply.github.com>
2024-02-20 21:22:11 -0500

    separate commands to different functions

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-def main():
-    with open("books/frankenstein.txt") as f:
+def get_book_content(link:str) -> str:
+    with open(link) as f:
         file_contents = f.read()
-        print(file_contents)
+
+    return file_contents
+    
+def count_words(words:str) -> int:
+    return len(words.split())
+
+def main():
+    content = get_book_content("books/frankenstein.txt")
+    print(content)
+    print(count_words(content))
 
 if __name__ == "__main__":
     main()
