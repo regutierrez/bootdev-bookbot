27097aac0cf78d5c01b74a32c0f93c40ea2f9168
regutierrez <11924623+regutierrez@users.noreply.github.com>
2024-02-23 20:07:41 -0500

    refactor get_char_count() to use Counter() and sort dict to descending order

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import Counter
 def get_book_content(link:str) -> str:
     with open(link) as f:
         file_contents = f.read()
@@ -7,10 +8,11 @@ def get_book_content(link:str) -> str:
 def count_words(words:str) -> int:
     return len(words.split())
 
-def count_letters(words:str) -> dict[str, int]:
+def get_char_count(words:str) -> dict[str, int]:
     words = words.lower()
+    """
+    # classical solution
     letters_count_dict:dict[str, int] = {}
-
     for char in words:
         if char in letters_count_dict:
             letters_count_dict[char] += 1
@@ -18,12 +20,36 @@ def count_letters(words:str) -> dict[str, int]:
             letters_count_dict[char] = 1
 
     return letters_count_dict
+    """
+
+    # we use this solution for easier sorting of the values
+    char_count_dict: dict[str, int] = Counter(words) # this does the counting for us
+    letter_count_dict: dict[str, int] = {}
+
+    # return only items that are in the alphabet
+    for char_item in char_count_dict:
+        if ord(char_item) >= ord('a') and ord(char_item) <= ord('z'):
+            letter_count_dict[char_item] = char_count_dict[char_item]
+
+    """
+    We use 'sorted' using the following arguments:
+    - key=lambda x:x[1] -> to sort dict according to its values (thus using 1 as index)
+    ** not sure why lambda is used tho ** 
+    - reverse=True -> sort in descending order
+    
+    But the output of that function is a list of tuples list[tuples(key,value)]
+    That's why we re-convert it to a dictionary using dict()
+    """
+    letter_count_dict = dict(sorted(letter_count_dict.items(), reverse=True, key=lambda x:x[1]))
+
+    return letter_count_dict
 
 def main():
-    content = get_book_content("books/frankenstein.txt")
-    # print(content)
-    # print(count_words(content))
-    print(count_letters(content))
+    filepath = "books/frankenstein.txt"
+    print(f'--- Begin report of {filepath} ---')
+    content = get_book_content(filepath)
+    print(f'{count_words(content)} words found in the document]\n\n')
+    print(get_char_count(content))
 
 if __name__ == "__main__":
     main()
