4c6c2cbf8f51e8cddd0af9669c27e9734b13dacf
regutierrez <11924623+regutierrez@users.noreply.github.com>
2024-02-23 19:18:45 -0500

    add count_letters fcn

--- main.py
+++ main.py
@@ -7,10 +7,23 @@ def get_book_content(link:str) -> str:
 def count_words(words:str) -> int:
     return len(words.split())
 
+def count_letters(words:str) -> dict[str, int]:
+    words = words.lower()
+    letters_count_dict:dict[str, int] = {}
+
+    for char in words:
+        if char in letters_count_dict:
+            letters_count_dict[char] += 1
+        else:
+            letters_count_dict[char] = 1
+
+    return letters_count_dict
+
 def main():
     content = get_book_content("books/frankenstein.txt")
-    print(content)
-    print(count_words(content))
+    # print(content)
+    # print(count_words(content))
+    print(count_letters(content))
 
 if __name__ == "__main__":
     main()
